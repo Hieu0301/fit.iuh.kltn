0d82e6f8bc680aadcf1fdfe160053a846f92c177
Hieu0301 <phamminhhieu.03012002@gmail.com>
2024-12-18 22:17:19 +0700

    deploy 1

--- client/src/firebase/ConfigFirebase.js
+++ client/src/firebase/ConfigFirebase.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBT8eLAjKP4Z7BQHM0aP7Atk_FUDfknpHo",
-  authDomain: "it-iuh-thesis-management.firebaseapp.com",
-  projectId: "it-iuh-thesis-management",
-  storageBucket: "it-iuh-thesis-management.appspot.com",
-  messagingSenderId: "826415557007",
-  appId: "1:826415557007:web:253b963c83e5bff93183fa",
-  measurementId: "G-54HJ29111N",
-  databaseURL:
-    "https://it-iuh-thesis-management-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  apiKey: "AIzaSyAFExzsU-fqeg4uYxh1Z42tvZcF0vE6iQ4",
+  authDomain: "websitekltn-24767.firebaseapp.com",
+  projectId: "websitekltn-24767",
+  storageBucket: "websitekltn-24767.firebasestorage.app",
+  messagingSenderId: "124870605581",
+  appId: "1:124870605581:web:89e66dd54ec11b01c50e44",
+  databaseURL: "https://websitekltn-24767-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 
 // Khởi tạo Firebase
